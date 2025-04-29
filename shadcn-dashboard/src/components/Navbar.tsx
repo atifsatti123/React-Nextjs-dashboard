524ea86c433f8a6012676e23bc249908dafa4ea5
atif-maxgo123 <atif.maxgo@gmail.com>
2025-04-29 14:34:13 +0500

    sidebar and nested sidebar completed

--- shadcn-dashboard/src/components/Navbar.tsx
+++ shadcn-dashboard/src/components/Navbar.tsx
@@ -12,12 +12,15 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { useTheme } from "next-themes";
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
+  const { toggleSidebar } = useSidebar();
   return (
     <nav className="p-4 flex items-center justify-between">
-      collapseButton
+      <SidebarTrigger />
+      {/* <Button variant="outline" onClick={toggleSidebar}></Button> */}
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
         <DropdownMenu>

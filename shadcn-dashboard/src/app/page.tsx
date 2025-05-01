d02b59c63ec2f864da0186c22d0e1eea8ecef61e
atif-maxgo123 <atif.maxgo@gmail.com>
2025-05-01 12:20:36 +0500

    create charts

--- shadcn-dashboard/src/app/page.tsx
+++ shadcn-dashboard/src/app/page.tsx
@@ -1,6 +1,25 @@
+"use client";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 import { Button } from "@/components/ui/button";
 import { CirclePlus } from "lucide-react";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+    </div>
+  );
 }

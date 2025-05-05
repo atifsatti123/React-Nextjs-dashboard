89299d734f45b25605e879c911567548e6be2433
atif-maxgo123 <atif.maxgo@gmail.com>
2025-05-05 18:50:05 +0500

    complete cards and user page and edit user page

--- shadcn-dashboard/src/app/page.tsx
+++ shadcn-dashboard/src/app/page.tsx
@@ -3,6 +3,7 @@ import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 import { Button } from "@/components/ui/button";
 import { CirclePlus } from "lucide-react";
 
@@ -18,7 +19,9 @@ export default function Home() {
       <div className="bg-primary-foreground p-4 rounded-lg">
         <AppPieChart />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <TodoList />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppAreaChart />
       </div>
---
+++ shadcn-dashboard/src/app/users/page.tsx
@@ -0,0 +1,5 @@
+const UsersPage = () => {
+  return <div>User page</div>;
+};
+
+export default UsersPage;
